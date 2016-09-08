8f9cd695d71b8c52688425db5fa77b45aa2173ab
jaronoff97 <jacobaronoff45@gmail.com>
2016-09-08 12:16:01 -0400

    syntax

--- app.py
+++ app.py
@@ -8,13 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')  # serve at root of website
 def home():
-    # parse hours, minutes, and seconds from sunset time
-    # get the current hours, minutes, and seconds
-    # sunset_hours = current_hours # debugging
-    # current_minutes = sunset_minutes - 5 # debugging
-    # if PM (because sunset) and correct hour, and is wednesday
     if isSunset():
-        # if within 10 minutes of sthacks
         location = choice(locations)
         return render_template('base.html', location=location), 200
     else:
