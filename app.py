4f3c21ae878ab927e4c1460d2a07c928ce4ac7c8
David Dworken <david@daviddworken.com>
2016-09-07 23:19:02 -0400

    Open example HTML template and location file to get location

--- app.py
+++ app.py
@@ -18,17 +18,25 @@ def home():
     (current_hours, current_minutes) = (currentTime.hour,
                                         currentTime.minute)
     # get the current hours, minutes, and seconds
-    # sunset_hours = Chours # debugging
-    # Cminutes = sunset_minutes - 5 # debugging
+    #sunset_hours = current_hours # debugging
+    #current_minutes = sunset_minutes - 5 # debugging
     # if PM (because sunset) and correct hour, and is wednesday
     if 'PM' in time and sunset_hours == current_hours and currentTime.isoweekday() == 3:
         if sunset_minutes - 10 < current_minutes < sunset_minutes:
             # if within 10 minutes of sthacks
-            return "Sthacks!"
+            with open('location.txt') as f:
+                location = f.read()
+            print location
+            return getHTML().replace("LOCATION", location)
     else:
-        return "No sthacks"
+        return getHTML().replace("LOCATION", "Come back later")
 
 
+def getHTML():
+    with open('template.html') as f:
+        return f.read()
+
+    
 def getTimeOfSunset():  # returns time of sunset in the form of "11:33:27 PM"
     # hit up the API to get the time
     url = "http://api.sunrise-sunset.org/json?lat=42.340955&lng=-71.090507&date=today"
@@ -38,4 +46,4 @@ def getTimeOfSunset():  # returns time of sunset in the form of "11:33:27 PM"
     return apiResp["results"]["civil_twilight_end"]
 
 if __name__ == '__main__':  # only run if this is being run as the main app
-    app.run(port=80)
+    app.run(port=80, debug=True)
